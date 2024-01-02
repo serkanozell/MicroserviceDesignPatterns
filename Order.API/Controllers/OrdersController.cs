da17ad235f97836028c6ac9688c37ae1a09fa592
serkanozel399 <serkanozel399@gmail.com>
2024-01-02 16:06:06 +0300

    Order API added

---
+++ Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,79 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Order.API.DTOs;
+using Order.API.Models;
+using Shared;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public OrdersController(AppDbContext context, IPublishEndpoint publishEndpoint)
+        {
+            _context = context;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderCreateDto orderCreateDto)
+        {
+            var newOrder = new Models.Order
+            {
+                BuyerId = orderCreateDto.BuyerId,
+                Status = OrderStatus.Suspend,
+                Address = new Address
+                {
+                    Line = orderCreateDto.AddressDto.Line,
+                    Province = orderCreateDto.AddressDto.Province,
+                    District = orderCreateDto.AddressDto.District,
+                },
+                CreatedDate = DateTime.Now,
+            };
+
+            orderCreateDto.OrderItems.ForEach(item =>
+            {
+                newOrder.Items.Add(new OrderItem
+                {
+                    Price = item.Price,
+                    ProductId = item.ProductId,
+                    Count = item.Count,
+                });
+            });
+
+            await _context.AddAsync(newOrder);
+            await _context.SaveChangesAsync();
+
+            var orderCreatedEvent = new OrderCreatedEvent
+            {
+                BuyerId = orderCreateDto.BuyerId,
+                OrderId = newOrder.Id,
+                Payment = new PaymentMessage
+                {
+                    CardName = orderCreateDto.Payment.CardName,
+                    CardNumber = orderCreateDto.Payment.CardNumber,
+                    Expiration = orderCreateDto.Payment.Expiration,
+                    CVV = orderCreateDto.Payment.CVV,
+                    TotalPrice = orderCreateDto.OrderItems.Sum(oi => oi.Price * oi.Count)
+                },
+            };
+
+            orderCreateDto.OrderItems.ForEach(item =>
+            {
+                orderCreatedEvent.OrderItemMessages.Add(new OrderItemMessage
+                {
+                    Count = item.Count,
+                    ProductId = item.ProductId
+                });
+            });
+
+            await _publishEndpoint.Publish(orderCreatedEvent);
+
+            return Ok();
+        }
+    }
+}
---
+++ Order.API/Program.cs
@@ -0,0 +1,42 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((contexti, cfg) =>
+    {
+        cfg.Host(host: builder.Configuration.GetConnectionString("RabbitMQ"));
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
